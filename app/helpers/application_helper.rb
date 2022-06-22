dd558f3bb455f5956db335551b3602cd44a7f492
ivanigorevich <ivanigorevich@github.com>
2022-06-22 10:29:47 +0400

    add inclination

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def inclination(number, word1, word2, word5)
+    return  word5 if (11..14).include?(number % 100)
+
+    last_digit = number % 10
+
+    return  word1 if last_digit == 1
+
+    return  word2 if (2..4).include?(last_digit)
+
+    word5
+  end
 end
